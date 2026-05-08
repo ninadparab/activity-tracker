20d9ad9d0255d06507cda20e2d954d948e275da8
ninadparab <ninad.parab@gmail.com>
2026-05-07 21:56:58 -0700

    Change base path in Vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/family-activity-tracker/', // must match your GitHub repo name exactly
+  base: '/activity-tracker/', // must match your GitHub repo name exactly
 })
